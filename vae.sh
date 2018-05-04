4c61785e7176434de6cdb0b8a001f6f108539cfe
Michael Opara <opara@college.harvard.edu>
2018-05-04 00:10:02 -0400

    commit

---
+++ vae.sh
@@ -0,0 +1,7 @@
+alias clr='clear && printf "\e[3J"'
+alias src='echo "reloading /home/vae.sh"; source "/home/vae.sh"'
+alias d='source deactivate'
+alias v2='source activate vae'
+
+bind '"\t":menu-complete'
+bind 'set completion-ignore-case on'
